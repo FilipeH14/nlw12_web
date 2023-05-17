8612b9cf06df95c72423eabbfacf1fe21cb85fcf
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-05-17 08:01:06 -0300

    imports fonts on  frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/button"
+import { Button } from "@/components/Button";
+
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className='h-screen bg-zinc-950 p-6 text-zinc-50'>
+      <h1 className='text-4xl font-bold'>Hello world</h1>
+      <h1 className='text-4xl font-alt'>Capsula do tempo</h1>
       <Button />
     </div>
   )
